9c07015580ba0dd42823f2155088b9757ea0a8c7
shpaker <shpaker@gmail.com>
2017-01-03 20:56:47 +0300

    Logs off

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pyfeedstg
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
-                        filename="log.txt",
+                        # filename="log.txt",
                         format='%(asctime)s [%(levelname)-8s] %(message)s')
     logging.info('\n\n======== RUN SERVER ========\n')
     pyfeedstg.Server().run()
