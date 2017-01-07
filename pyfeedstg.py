9212c5671e07d8a911a36232d74d154eab43d51b
shpaker <shpaker@gmail.com>
2017-01-07 22:36:26 +0300

    remove prepareText and add string's Template parsing

--- pyfeedstg.py
+++ pyfeedstg.py
@@ -1,6 +1,7 @@
 import asyncio
 import html
 import logging
+from string import Template
 
 import feedparser
 import telepot
@@ -44,7 +45,7 @@ class FeedForwarder(object):
         self.url = url
         self.userId = userId
         self.delay = delay
-        self.customFormat = customFormat
+        self.format = customFormat
         self.telegramPreview = telegramPreview
         self.feed = feedparser.parse(url)
         self.title = self.feed['feed']['title']
@@ -54,7 +55,7 @@ class FeedForwarder(object):
     def getUpdates(self):
         newEntries = []
         updatedFeed = feedparser.parse(self.url)
-        if len(updatedFeed["entries"]) > 0:
+        if len(updatedFeed['entries']) > 0:
             logging.debug('Get {} records from "{}", old feed contains {}'
                           .format(len(updatedFeed['entries']),
                                   self.url,
@@ -73,11 +74,34 @@ class FeedForwarder(object):
             logging.warning('Get empty feed from "{}"'.format(self.url))
         return newEntries
 
+    def sendEntry(self, id, entry):
+        tags = []
+        if 'tags' in entry:
+            for tag in entry['tags']:
+                tags.append(tag['term'])
+
+        templateDict = dict(author=html.escape(entry['author']),
+                            description=html.escape(entry['summary']),
+                            tags=html.escape(', '.join(tags)),
+                            time=entry['published'],
+                            title=html.escape(entry['title']),
+                            url=entry['id'])
+
+        outputText = Template(self.customFormat).safe_substitute(templateDict)
+        logging.info('Send to "{}": "{}"'.format(id, outputText))
+        disablePagePreview = not self.telegramPreview
+        try:
+            self.bot.sendMessage(id,
+                                 outputText,
+                                 parse_mode='HTML',
+                                 disable_web_page_preview=disablePagePreview)
+        except Exception as err:
+            logging.warning(err)
+
     async def run(self):
         logging.info('Start listening "{}" on "{}"'.format(self.title,
                                                            self.url))
-        self.sendTelegram(self.userId,
-                          self.prepareMessage(self.feed["entries"][0]))
+        self.sendEntry(self.userId, self.feed["entries"][0])
         while True:
             await asyncio.sleep(self.delay)
             rssNewEntries = self.getUpdates()
@@ -85,32 +109,4 @@ class FeedForwarder(object):
                 logging.info('Get {} new entries from "{}"'.
                              format(len(rssNewEntries), self.url))
             for entry in rssNewEntries:
-                    self.sendTelegram(self.userId,
-                                      self.prepareMessage(entry))
-
-    def prepareMessage(self, entry):
-        text = self.customFormat
-        logging.info("Prepare to sending entry {}".format(entry["id"]))
-        text = text.replace('$url$', entry['id'])
-        text = text.replace('$title$', html.escape(entry['title']))
-        text = text.replace('$time$', entry["published"])
-        text = text.replace('$description$', html.escape(entry['summary']))
-        if 'author' in entry:
-            text = text.replace('$author$', html.escape(entry['author']))
-        if 'tags' in entry:
-            tags = []
-            for tag in entry['tags']:
-                tags.append(tag['term'])
-            text = text.replace('$tags$', html.escape(', '.join(tags)))
-        return text
-
-    def sendTelegram(self, id, text, textFormat='HTML'):
-        logging.debug('Sending to {}: "{}"'.format(id, text))
-        disablePagePreview = not self.telegramPreview
-        try:
-            self.bot.sendMessage(id,
-                                 text,
-                                 parse_mode=textFormat,
-                                 disable_web_page_preview=disablePagePreview)
-        except Exception as err:
-            logging.warning(err)
+                    self.sendEntry(self.userId, entry)
