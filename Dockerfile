cef7ab3634fd592da0724e62659c30159b58989c
shpaker <shpaker@gmail.com>
2019-12-30 16:38:09 +0100

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,19 @@
 FROM python:3.7-alpine
 
 # env vars
-ENV FEEDS_PATH='feeds.yml'
+ENV ENV_CONFIGURATION=True \
+    FEEDS_PATH=/feedforbot/feeds.yml
 
-# add files anf user
-RUN adduser -D -h /home/feedforwarder feedforwarder
-WORKDIR /home/feedforwarder
+# add files and user
+RUN adduser -D -h /feedforbot feedforbot
+WORKDIR /feedforbot
 
 # setup requirements
 ADD requirements.txt requirements.txt
 RUN pip install --disable-pip-version-check --requirement requirements.txt
 
 # execute from user
-USER feedforwarder
-
+USER feedforbot
 ADD ./src .
 
 ENTRYPOINT ["python", "app.py"]
--- src/bot/main.py
+++ src/bot/main.py
@@ -1,10 +1,11 @@
 import logging
+from dataclasses import asdict
+from pprint import pformat
 from typing import Optional
 
 from .bot import Bot
 from .core import Settings
 
-DEFAULT_REDIS_HOST = '127.0.0.1'
 DEFAULT_REDIS_PORT = 6379
 DEFAULT_IS_DEBUG = False
 DEFAULT_TG_PROXY = None
@@ -12,7 +13,7 @@ DEFAULT_TG_PROXY = None
 
 def main(tg_token: str,
          feeds_path: str,
-         redis_host: str = DEFAULT_REDIS_HOST,
+         redis_host: str,
          redis_port: int = DEFAULT_REDIS_PORT,
          is_debug: bool = DEFAULT_IS_DEBUG,
          tg_proxy: Optional[str] = DEFAULT_TG_PROXY) -> None:
@@ -25,8 +26,12 @@ def main(tg_token: str,
                         feeds_path=feeds_path)
 
     log_level = logging.DEBUG if settings.is_debug else logging.INFO
-
     logging.basicConfig(format=settings.log_format,
                         level=log_level)
+
+    settings_dict = asdict(settings)
+    settings_dict.pop('tg_token')
+    logging.info(f'Settings:\n{pformat(settings_dict)}')
+
     with Bot(settings=settings) as bot:
         bot.run()
